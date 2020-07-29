040c0a402ae8b26ff8b7082ee28678757157b189
csynoers <3s0c9m7@gmail.com>
2020-07-29 16:32:55 +0700

    add detail book

--- src/js/routes.js
+++ src/js/routes.js
@@ -4,7 +4,6 @@ import AboutPage from '../pages/about.f7.html';
 import FormPage from '../pages/form.f7.html';
 import RegisterMember from '../pages/register-member.f7.html';
 
-
 import BooksDetail from '../pages/books-detail.f7.html';
 import DynamicRoutePage from '../pages/dynamic-route.f7.html';
 import RequestAndLoad from '../pages/request-and-load.f7.html';
@@ -14,53 +13,6 @@ var routes = [
   {
     path: '/',
     component: HomePage,
-    // async: function (routeTo, routeFrom, resolve, reject) {
-    //   // Router instance
-    //   var router = this;
-
-    //   // App instance
-    //   var app = router.app;
-
-    //   // Show Preloader
-    //   app.preloader.show();
-    //   // User ID from request
-    //   var userId = routeTo.params.userId;
-
-    //   // Simulate Ajax Request
-    //   setTimeout(function () {
-    //     // We got user data from request
-    //     var user = {
-    //       firstName: 'Vladimir',
-    //       lastName: 'Kharlampidi',
-    //       about: 'Hello, i am creator of Framework7! Hope you like it!',
-    //       links: [
-    //         {
-    //           title: 'Framework7 Website',
-    //           url: 'http://framework7.io',
-    //         },
-    //         {
-    //           title: 'Framework7 Forum',
-    //           url: 'http://forum.framework7.io',
-    //         },
-    //       ]
-    //     };
-
-    //     // Hide Preloader
-    //     app.preloader.hide();
-
-    //     // Resolve route to load page
-    //     resolve(
-    //       {
-    //         component: HomePage,
-    //       },
-    //       {
-    //         context: {
-    //           user: user,
-    //         }
-    //       }
-    //     );
-    //   }, 1000);
-    // },
   },
   {
     path: '/book/:bookId',
